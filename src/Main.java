e54c5a6349e4d628464625c1cd1454c655e95abc
ericdenser <ericdenser7@gmail.com>
2025-07-02 18:05:03 -0300

    Agora o programa mostra o carrinho quando digita 0
    - Troquei o System.exit por return pra encerrar de um jeito mais simples
    - Retorna uma mensagem se nenhum produto for comprado
    - Programa encerra apos a visualizacao do carrinho

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import model.Produto;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import static java.lang.System.exit;
-
 public class Main {
     public static void main(String[] args) {
 
@@ -13,7 +11,7 @@ public class Main {
 
         System.out.println("Bem vindo ao mercado lilo");
         while (true) {
-            System.out.print("Informe o produto que comprou (0 para sair): ");
+            System.out.print("Informe o produto que comprou (0 para ir ao carrinho): ");
             String inputNome = read.nextLine();
 
             if (inputNome.equals("0")) {
@@ -37,5 +35,26 @@ public class Main {
             listaDeProdutos.add(produto);
             System.out.println(produto.toString());
         }
+
+        double valorCarrinho = 0;
+
+        System.out.println("\n========CARRINHO========");
+
+        if (listaDeProdutos.isEmpty()) {
+            System.out.println("Parece que você não comprou nenhum produto :(");
+            return;
+        }
+
+        for (Produto p: listaDeProdutos) {
+            System.out.print(p);
+            valorCarrinho += p.getSubTotal();
+        }
+        System.out.printf("Valor total a pagar: R$%.2f", valorCarrinho);
+        System.out.println("\n========================");
+        System.out.println("""
+                \nFelizmente estamos oferecendo aos primeiros clientes todos os produtos de graca!
+                Fique ligado para novas atualizacoes e obrigado por experimentar!
+                """);
+        return;
     }
 }
